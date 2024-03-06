4887a95fd9451a0239a84ea494ee11ec88e207d7
Sujith HOC <sujith@houseofcomms.com>
2024-03-06 17:25:59 +0400

    split the payment link into another para

--- resources/views/emails/parent.blade.php
+++ resources/views/emails/parent.blade.php
@@ -55,9 +55,9 @@
     <li><b>What NOT to bring:</b> We kindly ask for no electronics or toys / Pokemon cards etc. to be bought to camp, as we will focus on sport for the duration of the day and want to ensure no children lose / damage their belongings.</li>
 </ul>
 
-<p style="padding: 5px;background-color: #FFFF00;font-weight: bold;font-size:18px;font-weight: bold;max-width: 630px;">
+<p style="padding: 5px;font-size:18px;font-weight: bold;">
     @if($paymentLink)
-        <b>To complete your payment and registration, please follow the link <a href="{{ $paymentLink }}" style="color: blue;">here</a></b>.
+        <span style="padding: 5px;background-color: #FFFF00;">To complete your payment and registration, please follow the link </span><a href="{{ $paymentLink }}" style="color: blue;">here</a>
     @else
         <b>Our Team will be in touch to advise how to make payment</b>.
     @endif
